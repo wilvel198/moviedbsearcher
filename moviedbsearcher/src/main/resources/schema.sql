c45dfb5193910eda78dc94156ff0ecf797f900ae
wilvel198 <39566372+wilvel198@users.noreply.github.com>
2019-05-21 20:24:51 -0500

    Update schema.sql

--- moviedbsearcher/src/main/resources/schema.sql
+++ moviedbsearcher/src/main/resources/schema.sql
@@ -1,1 +1,12 @@
+DROP TABLE IF EXISTS MovieData;
 
+CREATE TABLE MovieData (
+  id          INTEGER PRIMARY KEY,
+  fullJson VARCHAR(250) ,
+  movieID VARCHAR(220),
+  popularity VARCHAR(220),
+  release_date VARCHAR(220),
+  spoken_languages VARCHAR(220),
+  title VARCHAR(220),
+
+);
